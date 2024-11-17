eb54af2e110d68d06ce6b4e17fc3c1c01006495a
yony-k <109204976+yony-k@users.noreply.github.com>
2024-11-18 00:55:50 +0900

    [level 2] Title: 카펫, Time: 0.10 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,16 +1,19 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
+        int extent = brown + yellow;
         
-        for(int i=1; i<=Math.sqrt(yellow); i++) {
-            if(yellow % i == 0) {
-                int width = yellow / i;
+        for(int i=1; i<=Math.sqrt(extent); i++) {
+            
+            if(extent % i == 0) {
+                int width = extent / i;
                 int length = i;
-                int calcul = (width*2) + (length*2) + 4;
-                
+                int calcul = (width*2) + ((length-2)*2);
+
                 if(calcul == brown) {
-                    answer[0] = width+2;
-                    answer[1] = length+2;
+                    answer[0] = width;
+                    answer[1] = length;
+                    break;
                 }
             }
         }
